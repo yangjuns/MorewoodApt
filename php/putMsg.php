634e885e8c3a424b2580a3b1716a2d7108ad2a60
Yangjun Sheng <yangjuns@andrew.cmu.edu>
2017-09-14 18:21:02 -0400

    Fix words encoding

--- php/putMsg.php
+++ php/putMsg.php
@@ -4,6 +4,7 @@ $userId = $_POST["person"];
 $msg = $_POST["msg"];
 
 $conn = mysqli_connect("yangjuns.info", "root", "qweasdzxc", "morewoodapt");
+mysqli_query($conn, "SET NAMES utf8;");
 $query = "INSERT INTO comments (userid, comments, active, time) VALUES ($userId, '$msg' , 1, NOW())";
 mysqli_query($conn, $query);
 
