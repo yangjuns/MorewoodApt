bc2de532042dee26e8a0fc4c273ef2c15a8d5611
Yangjun Sheng <yangjuns@andrew.cmu.edu>
2017-09-14 20:23:22 -0400

    Implement comment deletion

--- php/delMsg.php
+++ php/delMsg.php
@@ -1,15 +1,13 @@
 <?php
 
-$userId = $_POST["person"];
-$msg = $_POST["msg"];
+$id = $_POST["commentid"];
+
 $dbname = "morewoodapt";
 //$dbname = "morewoodapt_test";
 
 $conn = mysqli_connect("yangjuns.info", "root", "qweasdzxc", $dbname);
 mysqli_query($conn, "SET NAMES utf8;");
-$query = "INSERT INTO comments (userid, comments, active, time) VALUES ($userId, '$msg' , 1, NOW())";
-mysqli_query($conn, $query);
-
+$query = "UPDATE comments SET active = 0 WHERE commentid = $id";
+$result = mysqli_query($conn, $query);
 mysqli_close($conn);
-
 ?>
--- php/getMsg.php
+++ php/getMsg.php
@@ -1,11 +1,12 @@
 <?php
-
-$val = $_POST["key"];
 $msgLimit = $_POST["limit"];
 
-$conn = mysqli_connect("yangjuns.info", "root", "qweasdzxc", "morewoodapt");
+$dbname = "morewoodapt";
+//$dbname = "morewoodapt_test";
+
+$conn = mysqli_connect("yangjuns.info", "root", "qweasdzxc", $dbname);
 mysqli_query($conn, "SET NAMES utf8;");
-$query = "SELECT time, firstname, comments FROM users AS u, comments AS c WHERE u.userid = c.userid ORDER BY time DESC LIMIT $msgLimit";
+$query = "SELECT commentid, time, firstname, comments FROM users AS u, comments AS c WHERE u.userid = c.userid AND active = 1 ORDER BY time DESC LIMIT $msgLimit";
 $result = mysqli_query($conn, $query);
 $msgs = array();
 date_default_timezone_set("US/Eastern");
