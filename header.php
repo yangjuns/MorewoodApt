e234d4dfb62ee9ad1479763f177bc1aedcf360af
Yangjun Sheng <yangjuns@andrew.cmu.edu>
2017-09-18 23:54:43 -0400

    Add about me page

--- header.php
+++ header.php
@@ -6,7 +6,7 @@ if (!empty($_SESSION["username"])) {
     <li class="dropdown">
         <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{$_SESSION["username"]}<span class="caret"></span></a>
         <ul class="dropdown-menu">
-        <li><a>About Me</a></li>
+        <li><a href="/user.php">About Me</a></li>
         <li role="separator" class="divider"></li>
         <li><a href="/logout.php">Logout</a></li>
         </ul>
